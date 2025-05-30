2d7dccc88ea2bfe1b6bbca6b971a1375727f9a81
tea-aroma <kovalyovictor71@gmail.com>
2025-05-30 23:17:35 +0500

    feat: add new properties in 'CacheTag' enum; add the new 'cart' helper.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -10,3 +10,14 @@ if (! function_exists('user'))
         return \App\Data\Users\UserData::fromModel(request()->user());
     }
 }
+
+if (! function_exists('cart'))
+{
+    /**
+     * @return \App\Standards\Cart\Cart
+     */
+    function cart(): \App\Standards\Cart\Cart
+    {
+        return new \App\Standards\Cart\Cart();
+    }
+}
